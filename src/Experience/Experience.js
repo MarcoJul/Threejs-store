e39049c479ed0da301a6eb71bf9a878dc42a3051
marco <marco.giuliotti@gmail.com>
2023-02-20 16:28:08 +0100

    Add Time class and update method

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,23 +1,29 @@
 import Sizes from "../Utils/Sizes";
+import Time from "../Utils/Time";
 
 export default class Experience {
   constructor(canvas){
     this.canvas = canvas;
 
     this.sizes = new Sizes();
+    this.time = new Time();
 
     // Sizes resize event
     this.sizes.on("resize", () => {
       this.resize();
-  });
-  }
+    }); 
 
+    this.time.on("tick", () =>{
+      this.update();
+    })
+  
+  }
 
   resize(){
 
   }
 
   update(){
-
+    
   }
 }
