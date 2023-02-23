9f665d884ccd3242ffa560c8cbca206b7ea876c0
marco <marco.giuliotti@gmail.com>
2023-02-23 18:33:50 +0100

    Add Orbitcontrols to camera and resize-update methods

--- src/Camera.js
+++ src/Camera.js
@@ -19,4 +19,18 @@ export default class Camera {
     this.instance.position.set(6,4,8);
     this.scene.add(this.instance);
   }
+
+  setOrbitControls() {
+    this.controls = new OrbitControls(this.instance, this.canvas);
+    this.controls.enableDamping = true;
+  }
+
+  resize() {
+    this.instance.aspect = this.sizes.width / this.sizes.height;
+    this.instance.updateProjectionMatrix();
+  }
+
+  update() {
+    this.controls.update();
+  }
 }
